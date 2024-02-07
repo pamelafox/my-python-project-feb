2d5c5e878ede539bf019ba4aadbe4a14cf351edf
Pamela Fox <pamela.fox@gmail.com>
2024-02-07 10:24:33 -0800

    Made cool changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from image_helpers import image_helpers
+
+
 import image_filters
 
+import numpy
 
 # Use the functions above to fetch pixel data and render the original data
-url = "https://www2.eecs.berkeley.edu/Faculty/Photos/Homepages/pamelafox.jpg"
+url = 'https://www2.eecs.berkeley.edu/Faculty/Photos/Homepages/pamelafox.jpg'
 pixel_data = image_helpers.get_image_pixels(url)
 # image_helpers.render_pixels(pixel_data)
-image_helpers.render_pixels(image_filters.remove_red(pixel_data))
+image_helpers.render_pixels(
+    image_filters.remove_red(
+        pixel_data))
